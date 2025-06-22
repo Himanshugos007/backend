67237e07ec9dcb37002e47153cb3287d3a7c4c2a
himanshu <himanshugoswami@himanshus-MacBook-Air.local>
2025-06-22 11:32:22 +0530

    Enable CORS second

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors({
   origin: "https://benevolent-melomakarona-5e0934.netlify.app",
